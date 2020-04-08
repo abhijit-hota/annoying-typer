952aa9040d7a095b2f35b58631b25d05ea0107cb
Abhijit Hota <abhihota025@gmail.com>
2020-04-08 18:45:25 +0530

    Update script.js
    Removed Spanish text

--- script.js
+++ script.js
@@ -2,39 +2,30 @@
 $(document).ready(function(){
 ///////The random annoying texts////////
 var texts = [{eng:"Pressing any key won't help", 
-   span:"Al pulsar cualquier tecla no ayudará",
    img:"﴾͡๏̯͡๏﴿ O'RLY?"
   }, 
   {eng:"You're screwed up man.",
-   span:"Estás jodido hombre.",
    img:"〆(・∀・＠)"
   },
-  {eng:"Oh please, don't try to hack me.",
-  span:"Oh, por favor, no trates de cortar.",
+  {eng:"Oh please, don't try to hack me."
   img:"◔̯◔"
   }, 
   {eng:"I has you.",
-   span:"Te tuve.",
    img:"╚(ಠ_ಠ)=┐"
   },
-  {eng:"Buy yourself some M&Ms.",
-  span:"Cómprate algo de M&Ms.",
+  {eng:"Buy yourself some M&Ms."
    img:"[̲̅$̲̅(̲̅ ͡° ͜ʖ ͡°̲̅)̲̅$̲̅]"
   },
   {eng:"You got some bucks?",
-   span:"¿Tienes algo de dinero?",
    img:"[̲̅$̲̅(̲̅5̲̅)̲̅$̲̅]"
   }, 
   {eng:"Whatever.",
-   span:"Lo Que Sea.",
    img:"¯\_(ツ)_/¯"
   }, 
   {eng:"Did you hear uptown funk?",
-   span:"¿Has oído el funk de la parte alta?",
    img:"ヾ(⌐■_■)ノ♪"
   },
   {eng:"ERROR_404. BRAIN NOT FOUND IN THIS MECHANICAL DEVICE. NOT A TERMINATOR",
-   span:"ERROR_404. CEREBRO NO ENCONTRADA EN ESTE DISPOSITIVO MECÁNICO. NO A TERMINATOR",
    img:"┬─┬﻿ ︵ /(.□. ）"
   }];
   
